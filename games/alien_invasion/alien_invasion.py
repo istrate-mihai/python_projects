130fde0eefa5655124e0746984fdd72129740d14
istrate-mihai <i.mihai9960@gmail.com>
2026-06-19 20:51:42 +0300

    feat: completed main alien invasion game functionalities, ship movement + bullets firing + alien fleet generation + collison

--- games/alien_invasion/alien_invasion.py
+++ games/alien_invasion/alien_invasion.py
@@ -1,5 +1,7 @@
 import pygame
+from pygame.sprite import Group
 from settings import Settings
+from game_stats import GameStats
 from ship import Ship
 import game_functions as gf
 
@@ -13,15 +15,28 @@ def run_game():
         (ai_settings.screen_width, ai_settings.screen_height)
     )
 
-    # Make a ship
-    ship = Ship(screen)
+    # Make a ship, a group of bullets, and a group of aliens.
+    ship = Ship(ai_settings, screen)
+    bullets = Group()
+    aliens = Group()
 
     pygame.display.set_caption("Alien Invasion")
+    # Create an instance to store game statistics.
+    stats = GameStats(ai_settings)
 
+    # Create the fleet of aliens
+    gf.create_fleet(ai_settings, screen, ship, aliens)
+
+    # Start the main loop for the game
     while True:
+        gf.check_events(ai_settings, screen, ship, bullets)
 
-        gf.check_events(ship)
-        gf.update_screen(ai_settings, screen, ship)
+        if stats.game_active:
+            ship.update()
+            gf.update_bullets(ai_settings, screen, ship, aliens, bullets)
+            gf.update_aliens(ai_settings, stats, screen, ship, aliens, bullets)
+
+        gf.update_screen(ai_settings, screen, ship, aliens, bullets)
 
 
 run_game()
